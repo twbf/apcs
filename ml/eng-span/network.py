29e90c5ce808e1c933c588484d7b2460983e91e6
Thomas Bueler <twbueler@gmail.com>
2018-07-25 22:07:01 -0800

    first step in str to binary. Delited <?>

--- ml/eng-span/network.py
+++ ml/eng-span/network.py
@@ -1,7 +1,24 @@
 
-import tensorflow as tf
+
 import math
 import numpy as np
 from random import *
+
+english = open("/Users/thomasbueler-faudree/GitHub/apcs/ml/eng-span/english.txt", "r")
+spanish = open("/Users/thomasbueler-faudree/GitHub/apcs/ml/eng-span/spanish.txt", "r")
+eng = english.readlines()
+spn = spanish.readlines()
+
+
+def changeToBit(index, lang):
+    if lang == 1:  #english
+        for x in eng[index]:
+            print format(ord(x),'b')  # convert to binary starts at 97
+    else:          #spanish
+        print "h"
+
+changeToBit(4,1)
+
+print len(spn)
 
 print "hh"
