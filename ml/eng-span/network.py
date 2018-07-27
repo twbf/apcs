613fad4362676a2db70cf4415195b0e8f1209701
Thomas Bueler <twbueler@gmail.com>
2018-07-27 14:08:46 -0800

    Training!!!!

--- ml/eng-span/network.py
+++ ml/eng-span/network.py
@@ -32,26 +32,45 @@ s = np.array(np.zeros((20000,30,8)))
 for i in range(20000):
     b = changeToBit(i,1)
     e[i] = b
-    e[i].flatten()
+
     b = changeToBit(i,0)
     s[i] = b
-    s[i].flatten()
 
-e.flatten()
 
-print e[300]
+train_in = np.array(np.zeros((20000,30,8)))
+train_out = np.array(np.zeros((20000,1)))
+val_in = np.array(np.zeros((20000,30,8)))
+val_out = np.array(np.zeros((20000,1)))
 
-eng_train = e[:10000]
-eng_val = e[10000:20000]
-spn_train = eng[:10000]
-spn_val = eng[10000:20000]
+for x in range(0,10000,2):
+    train_in[x] = e[x]
+    train_out[x] = 0
+    train_in[x+1] = s[x]
+    train_out[x+1] = 1
+
+    val_in[x] = e[x+10000]
+    val_out[x] = 0
+    val_in[x+1] = s[x+10000]
+    val_out[x+1] = 1
+
+
 
 
 def network():
     model = keras.Sequential()
-    model.add(keras.layers.Flatten(140, 16))
+    model.add(keras.layers.Flatten(input_shape = (30,8)))
+    model.add(keras.layers.Dense(10, activation = tf.nn.relu))
     model.add(keras.layers.Dense(10, activation = tf.nn.relu))
     model.add(keras.layers.Dense(1, activation = tf.nn.sigmoid))
     model.summary()
+    model.compile(optimizer=tf.train.AdamOptimizer(),
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+    history = model.fit(train_in,
+                    train_out,
+                    epochs=40,
+                    batch_size=512,
+                    validation_data=(val_in, val_out),
+                    verbose=1)
 
 network()
