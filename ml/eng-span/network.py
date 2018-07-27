f1f17b91b833b9cd9b4ed9b21f00df336eee6fe2
Thomas Bueler <twbueler@gmail.com>
2018-07-26 17:02:20 -0800

    starting on machine learning. finished with stupid string to int

--- ml/eng-span/network.py
+++ ml/eng-span/network.py
@@ -1,5 +1,5 @@
-
-
+import tensorflow as tf
+from tensorflow import keras
 import math
 import numpy as np
 from random import *
@@ -14,11 +14,36 @@ def changeToBit(index, lang):
     bin = []
     if lang == 1:  #english
         for x in eng[index]:
-             bin.append(format(ord(x),'b'))  # convert to binary starts at 97
+             bin.append(list(format(ord(x),'b')))  # convert to binary starts at 97
+        bin = map(int, bin)
         return bin
     else:          #spanish
         for x in spn[index]:
-             bin.append(format(ord(x),'b'))  # convert to binary starts at 97
+             bin.append(list(format(ord(x),'b')))  # convert to binary starts at 97
         return bin
 
-print changeToBit(0,0)
+for i in range(20000):
+    eng[i] = changeToBit(i,0)
+    for j in range(len(eng[i])):
+        eng[i][j] = map (int, eng[i][j])
+        
+    spn[i] = changeToBit(i,0)
+    for j in range(len(spn[i])):
+        spn[i][j] = map (int, spn[i][j])
+
+print eng[0]
+
+eng_train = eng[:10000]
+eng_val = eng[10000:20000]
+spn_train = eng[:10000]
+spn_val = eng[10000:20000]
+
+
+def network():
+    model = keras.Sequential()
+    model.add(keras.layers.Embedding(140, 16))
+    model.add(keras.layers.Dense(10, activation = tf.nn.relu))
+    model.add(keras.layers.Dense(1, activation = tf.nn.sigmoid))
+    model.summary()
+
+network()
