3665fe1dc06041cb2cade65aa4935cd72205f024
Thomas Bueler <twbueler@gmail.com>
2018-07-27 19:55:14 -0800

    got to 86% acuracy

--- ml/eng-span/network.py
+++ ml/eng-span/network.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 from tensorflow import keras
 import math
 import numpy as np
-from random import *
+import random
 
 english = open("/Users/thomasbueler-faudree/GitHub/apcs/ml/eng-span/english.txt", "r")
 spanish = open("/Users/thomasbueler-faudree/GitHub/apcs/ml/eng-span/spanish.txt", "r")
@@ -12,7 +12,7 @@ spn = spanish.readlines()
 
 def changeToBit(index, lang):
     bin = []
-    test = np.array((np.zeros((30,8))))
+    test = np.array((np.zeros((25,8))))
     if lang == 1:  #english
         for x in range(len(eng[index])):
              bin = format(ord(eng[index][x]),'b')  # convert to binary starts at 97
@@ -26,8 +26,8 @@ def changeToBit(index, lang):
                  test[x][y] =  bin[y]
         return test
 
-e = np.array(np.zeros((20000,30,8)))
-s = np.array(np.zeros((20000,30,8)))
+e = np.array(np.zeros((20000,25,8)))
+s = np.array(np.zeros((20000,25,8)))
 
 for i in range(20000):
     b = changeToBit(i,1)
@@ -37,30 +37,36 @@ for i in range(20000):
     s[i] = b
 
 
-train_in = np.array(np.zeros((20000,30,8)))
+train_in = np.array(np.zeros((20000,25,8)))
 train_out = np.array(np.zeros((20000,1)))
-val_in = np.array(np.zeros((20000,30,8)))
+val_in = np.array(np.zeros((20000,25,8)))
 val_out = np.array(np.zeros((20000,1)))
 
-for x in range(0,10000,2):
-    train_in[x] = e[x]
-    train_out[x] = 0
-    train_in[x+1] = s[x]
-    train_out[x+1] = 1
+for x in range(10000):
+    r = random.random()
+    if r<0.5:
+        train_in[x] = e[x]
+        train_out[x] = 0
+    else:
+        train_in[x] = s[x]
+        train_out[x] = 1
 
-    val_in[x] = e[x+10000]
-    val_out[x] = 0
-    val_in[x+1] = s[x+10000]
-    val_out[x+1] = 1
+    r = random.random()
+    if r<0.5:
+        val_in[x] = e[x+10000]
+        val_out[x] = 0
+    else:
+        val_in[x] = s[x+10000]
+        val_out[x] = 1
 
 
 
 
 def network():
     model = keras.Sequential()
-    model.add(keras.layers.Flatten(input_shape = (30,8)))
-    model.add(keras.layers.Dense(10, activation = tf.nn.relu))
-    model.add(keras.layers.Dense(10, activation = tf.nn.relu))
+    model.add(keras.layers.Flatten(input_shape = (25,8)))
+    model.add(keras.layers.Dense(100, activation = tf.nn.relu))
+    model.add(keras.layers.Dense(5, activation = tf.nn.relu))
     model.add(keras.layers.Dense(1, activation = tf.nn.sigmoid))
     model.summary()
     model.compile(optimizer=tf.train.AdamOptimizer(),
@@ -68,8 +74,8 @@ def network():
               metrics=['accuracy'])
     history = model.fit(train_in,
                     train_out,
-                    epochs=40,
-                    batch_size=512,
+                    epochs=100,
+                    batch_size=10000,
                     validation_data=(val_in, val_out),
                     verbose=1)
 
