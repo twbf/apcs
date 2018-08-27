005f294d3f6c6a9eb33ce46101f7570b949291ad
Thomas Bueler <twbueler@gmail.com>
2018-08-27 08:24:59 -0800

    pp1 setup done

---
+++ chap3/pp1.java
@@ -0,0 +1,14 @@
+class Ec6{
+    public static void main(String args[]){
+        tree(3,4);
+        System.out.println('      *      ');
+        System.out.println('      *      ');
+        System.out.println('   *******   ');
+    }
+    public static void tree(int seg, int h){
+        for (int i = 0; i<seg; i++){
+
+        }
+        
+    }
+}
