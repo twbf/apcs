db80e459bd86595d34842f8a9af2d0b33e8a788b
Thomas Bueler <twbueler@gmail.com>
2018-08-28 07:53:35 -0800

    done with pp2

--- chap3/pp2.java
+++ chap3/pp2.java
@@ -3,7 +3,7 @@ class Pp2{
         double money = 1000;
         double deposit = 0;
         for(int i=0; i<25; i++){
-            System.out.println(str(i) + ' ,' + str(money) + ' ,' + str(money*1.065) + ' ' + str(deposit) + ' ' + str(money*1.065+deposit));
+            System.out.println(i + " ," + money + " ," + money*0.065 + " " + deposit + " " + money*1.065+deposit);
             money = money*1.065 + deposit;
             deposit = 100;
         }
