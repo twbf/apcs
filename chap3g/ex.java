168e7d85cfe640dff1f1f2de4ee196eb4b645eba
Thomas Bueler <twbueler@gmail.com>
2018-09-14 08:38:47 -0800

    done

--- chap3g/ex.java
+++ chap3g/ex.java
@@ -5,6 +5,7 @@ class Ex{
     public static void main(String args[]){
         int MEASURLine = 3;
         int NOTES = 4;
+        int noteSpace = 64;
         Note[] notes = {new Note("a"), new Note("a")};
         int len = notes.length;
         int LINES = findLines(len, NOTES, MEASURLine);
@@ -24,7 +25,9 @@ class Ex{
         int spaceing = size/(NOTES*MEASURLine);
         int notesInLine = NOTES*MEASURLine;
 
+        int xPosition = 0;
         for (int j = 0; j < LINES; j++){
+
             for (int i = 0; i<5; i++){
                 s.drawLine(15, 30 + space*i + j*100, 15+size, 30 + space*i + j*100);
             }
@@ -32,6 +35,7 @@ class Ex{
                 notesInLine = len-count;
             }
             for(int i= 0; i<notesInLine; i++){
+                xPosition+=notes[count].getLength()
                 if (i%4 == 0){
                     s.drawLine(15+i*spaceing, 20 + j*100 + 10, 15+i*spaceing, 20 + j*100 + space*5 - 7);
                 }
@@ -44,7 +48,10 @@ class Ex{
     }
 
     public static int findLines(int len, int notes, int measure){
-        //dummy
-        return 2;
+        double l = len;
+        double n = notes;
+        double m = measure;
+        double lines = l/(n*m);
+        return (int) (lines + 1);
     }
 }
