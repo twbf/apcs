c9c3ebc5984f4e9741ae6e1165f643a92cfcbd02
Thomas Bueler <twbueler@gmail.com>
2018-09-21 08:33:05 -0800

    working on notes

--- chap3g/music.java
+++ chap3g/music.java
@@ -1,9 +1,18 @@
 import jm.JMC;
 import jm.music.data.*;
-import jm.util.*;
+import jm.util.Play;
 
-class music {
-    public static void main(String args[]){
-        System.out.println("maybe");
-    }
+public class Music {
+
+        public static void main(String[] args) {
+            Note n = new Note(JMC.C4, JMC.WN);
+            Note y = new Note(JMC.C4, JMC.QN);
+            Note x = new Note(JMC.C4, JMC.QN);
+            Phrase phr = new Phrase();
+            phr.addNote(new Note(JMC.C4, JMC.WN));
+            phr.addNote(y);
+            phr.addNote(x);
+            Play.midi(phr);
+        }
+
 }
