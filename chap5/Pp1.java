318605f74b03900d2966c2fb89914e5ef2a56441
Thomas Bueler <twbueler@gmail.com>
2018-10-01 21:38:58 -0800

    done with pp1

--- chap5/Pp1.java
+++ chap5/Pp1.java
@@ -2,27 +2,31 @@ import java.util.Scanner;
 
 class Pp1{
     public static void main(String args[]){
+        System.out.println("String?");
+
+        // get the line in a scanner so splicing by spaces is easy
         Scanner console = new Scanner(System.in);
+        Scanner scanner = new Scanner(console.nextLine());
+
         String out = "";
-        System.out.println("Word?");
-        console.next();
-        while(!console.hasNext()){
-            System.out.println("Word?");
-            out += toPig(console.next()) + " ";
+        while(scanner.hasNext()){
+            out += toPig(scanner.next()) + " ";
         }
         System.out.println(out);
     }
 
     public static String toPig(String word){
         int count = 0;
-        while(checkVowels(word.charAt(count))){
+        while(checkVowels(word.charAt(count))){  // if a chacter is a vowel then add one to count
             count++;
         }
-        return word.substring(count) + word.substring(0, count) + "-ay";
+
+        // returns the word in pig latin
+        return word.substring(count) + "-" + word.substring(0, count) + "ay";
     }
 
     public static boolean checkVowels(char letter){
-        return letter!='a'&&letter!='e'&&letter!='i'&&letter!='o'&&letter!='u';
+        return letter!='a'&&letter!='e'&&letter!='i'&&letter!='o'&&letter!='u'; //checks if a charcter is a vowel
     }
 
 }
