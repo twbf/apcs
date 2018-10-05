f9f5ad5e3900b28f1dc4b0a07298ccc2bf70d7f1
Thomas Bueler <twbueler@gmail.com>
2018-10-05 08:38:04 -0800

    working on gui

--- chap5/Pp5.java
+++ chap5/Pp5.java
@@ -12,23 +12,35 @@ import java.awt.BorderLayout;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-
 class Pp5{
     public static void main(String args[]){
         //gui
         JFrame guiFrame = new JFrame();
         guiFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        guiFrame.setTitle("Example GUI");
+        guiFrame.setTitle("Rock Paper Scissors");
         guiFrame.setSize(300,250);
 
         JPanel pane = new JPanel();
 
-        JLabel label = new JLabel("My label");
-        label.setText(" ");
-        pane.add(label);
+        JLabel scoreLabel = new JLabel("score");
+        JLabel playLabel = new JLabel("play");
+        scoreLabel.setText(" ");
+        playLabel.setText(" ");
+        pane.add(scoreLabel);
+        pane.add(playLabel);
         guiFrame.add(pane);
 
         guiFrame.setVisible(true);
+
+        JButton rock=new JButton("rock");
+        rock.addActionListener(new ActionListener() {
+
+			      @Override
+			          public void actionPerformed(ActionEvent arg0) {
+					       int play = play(); // with rock
+                 points(play);
+			      }
+	      });
 
         //initialy setting the scores
         int human = 0;
@@ -53,10 +65,30 @@ class Pp5{
         System.out.println("Computer: " + computer + "    Human: " + human);
     }
 
-    public static int play(){
-        System.out.println("Rock-1 Paper-2 Sicors-3");
-        Scanner console = new Scanner(System.in);
-        int human = console.nextInt();  // new int
+    public static void points(int play){
+        if (play == 0){
+            //tie
+            //stationary peice
+        } else if (play == 1) {
+            playLabel.setText("You win");
+            //move peice
+        } else {
+            playLabel.setText("Computer Wins");
+            //move peice
+        }
+        //scoreLabel.setText("Computer: " + computer + "    Human: " + human);
+    }
+
+    public static void movePiece(int play){
+        int increment = 10;
+        //move peice each way
+        // if hits wall have a big splash screen.
+    }
+
+    public static int play(int human){
+        //System.out.println("Rock-1 Paper-2 Sicors-3");
+        //Scanner console = new Scanner(System.in);
+        //int human = console.nextInt();  // new int
         int computer = ThreadLocalRandom.current().nextInt(1, 4); // to make 1-3 inclusive
         if (human == computer){ // tie
             return 0;
