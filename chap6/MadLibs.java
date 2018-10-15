56280553ea33bdffd26ccea372a395763d2710ce
Thomas Bueler <twbueler@gmail.com>
2018-10-15 08:14:52 -0800

    done with madLibs

--- chap6/MadLibs.java
+++ chap6/MadLibs.java
@@ -3,22 +3,26 @@ import java.io.*;
 import java.util.*;
 
 class MadLibs{
-    public static void main(String args[]) throws FileNotFoundException{
-        File file = new File("libs.txt");
-        Scanner input = new Scanner(file);
-        List<String> list = new ArrayList<String>();
+    public static void main(String args[]) throws FileNotFoundException {
+        File file = new File("libs.txt"); // get file
+        Scanner input = new Scanner(file); // put it in a scanner
+
+        List<String> list = new ArrayList<String>(); // new list
 
         while (input.hasNextLine()) {
             String line = input.nextLine();
 
-            if(StringUtils.isAllUpperCase(line)){
+            if(line.toUpperCase().equals(line)){  // if the line is all upper case
                 System.out.println("What is a " + line + "?");
-                Scanner sc = new Scanner(System.in);
+                Scanner sc = new Scanner(System.in); // get the word
                 list.add(sc.next());
-            } else {
-                list.add(line)
+            } else {  // if the line is output
+                list.add(line);
             }
         }
+
+        // print
+        System.out.println(" ");
         for (int i = 0; i<list.size(); i++){
             System.out.println(list.get(i));
         }
