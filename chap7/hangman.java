d0ab4097004653c5a5bcc963aa9513fba1ac3504
Thomas Bueler <twbueler@gmail.com>
2018-10-18 11:52:02 -0800

    starting hangman

---
+++ chap7/hangman.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+
+class Pp{
+    public static void main(String args[]) throws FileNotFoundException{
+        //get file of words
+        //make a window
+        //list the letters
+
+        //play
+        String word = "and";
+        //add stuff
+        //
+    }
+
+}
