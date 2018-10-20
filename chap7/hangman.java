4d23aa7e61b1185a1f42136f5a8d07ed760ea873
Thomas Bueler <twbueler@gmail.com>
2018-10-19 19:06:56 -0800

    array of buttons

--- chap7/hangman.java
+++ chap7/hangman.java
@@ -1,17 +1,50 @@
 import java.util.Scanner;
 import java.io.*;
 import java.util.*;
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
+import java.lang.Math;
+import java.lang.*;
+import java.awt.Graphics2D;
+import java.awt.Frame;
+import javax.swing.*;
+import javax.swing.JPanel;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import java.awt.BorderLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
-class Pp{
+class Hangman{
     public static void main(String args[]) throws FileNotFoundException{
+
+        JFrame guiFrame = new JFrame();
+        guiFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // so it closes nicely
+        guiFrame.setTitle("Hangman");
+        guiFrame.setSize(300,250);
+
+        JPanel pane = new JPanel();
+        char[] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+
+        JButton[] buttons = new JButton[26];
+        for(int i = 0; i<26; i++){
+            buttons[i] = new JButton(String.valueOf(alphabet[i]));
+            pane.add(buttons[i]);
+        }
+
+        guiFrame.add(pane);
+
+        guiFrame.setVisible(true);
+        //buttons[0] = new JButton("0");
         //get file of words
-        //make a window
-        //list the letters
 
         //play
         String word = "and";
         //add stuff
-        //
+        //char guess =
+
     }
 
 }
