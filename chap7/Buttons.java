45166f0391d6593cbc5abadb062186bf94597bfc
Thomas Bueler <twbueler@gmail.com>
2018-10-23 09:27:57 -0800

    when button is clicked it proints to screen

--- chap7/Buttons.java
+++ chap7/Buttons.java
@@ -7,7 +7,7 @@ import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.border.*;
 
-public class Buttons extends JPanel implements WindowListener,ActionListener{
+public class Buttons extends JFrame implements WindowListener,ActionListener{
     public Buttons(){
         JFrame guiFrame = new JFrame();
         guiFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // so it closes nicely
@@ -26,4 +26,22 @@ public class Buttons extends JPanel implements WindowListener,ActionListener{
         guiFrame.add(pane);
         guiFrame.setVisible(true);
     }
+
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == buttons[0]){
+            System.out.println("d");
+        }
+    }
+
+    public void windowOpened(WindowEvent e) {}
+    public void windowActivated(WindowEvent e) {}
+    public void windowIconified(WindowEvent e) {}
+    public void windowDeiconified(WindowEvent e) {}
+    public void windowDeactivated(WindowEvent e) {}
+    public void windowClosed(WindowEvent e) {}
+    public void windowClosing(WindowEvent e) {
+        dispose();
+        System.exit(0);
+    }
+
 }
