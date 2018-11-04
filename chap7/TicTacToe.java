be2a5c72f23745d95a302ad747574bdc94e19e91
Thomas Bueler <twbueler@gmail.com>
2018-11-03 21:00:18 -0800

    starting on Tic

--- chap7/TicTacToe.java
+++ chap7/TicTacToe.java
@@ -6,7 +6,32 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.border.*;
 
-public class Hangman{
+// Make sure that all of the objects are global
+
+public class TicTacToe{
+
+    public JFrame frame;
+    public JPanel panel;
+    private Graphics g;
+
+
     public static void main(String args[]) throws FileNotFoundException{
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // so it closes nicely
+        frame.setTitle("Tic-Tac-Toe");
+        frame.setSize(1240,800);
+
+        for(int i = 0; i<26; i++){
+            buttons[i] = new JButton(i);
+            buttons[i].setPreferredSize(new Dimension(40, 20));
+        }
+    }
+
+    public static void x(int x, int y){
+        g.drawLine(600, 250, 600, 270);
+        g.drawLine(600, 250, 600, 270);
+    }
+
+    public static void o(int x, int y){
+        g.drawOval(x, y, 40, 40);
     }
 }
