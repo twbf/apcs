b601894fbb89457cdb6542266a20afa7bfc318a3
Thomas Bueler <twbueler@gmail.com>
2018-11-05 09:38:44 -0900

    starting on tic tac toe

--- chap7/TicTacToe.java
+++ chap7/TicTacToe.java
@@ -20,15 +20,19 @@ public class TicTacToe{
         frame.setTitle("Tic-Tac-Toe");
         frame.setSize(1240,800);
 
-        for(int i = 0; i<26; i++){
+        for(int i = 0; i<9; i++){
             buttons[i] = new JButton(i);
             buttons[i].setPreferredSize(new Dimension(40, 20));
+            panel.add(buttons[i]);
         }
+
+        
+
     }
 
     public static void x(int x, int y){
-        g.drawLine(600, 250, 600, 270);
-        g.drawLine(600, 250, 600, 270);
+        g.drawLine(x+10, y+10, x-10, y-10);
+        g.drawLine(x-10, y+10, x-10, y+10);
     }
 
     public static void o(int x, int y){
