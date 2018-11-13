a939a8d5f5e6bf5eba851ef3adf018440ea2c7a5
Thomas Bueler <twbueler@gmail.com>
2018-11-13 08:09:53 -0900

    tic tac toe works

--- chap7/TicTacToe.java
+++ chap7/TicTacToe.java
@@ -13,6 +13,7 @@ public class TicTacToe{
     public JFrame frame;
     public JPanel panel;
     private Graphics g;
+    private static int[] board;
 
     public TicTacToe(){
         JFrame frame = new JFrame();
@@ -20,10 +21,12 @@ public class TicTacToe{
         frame.setTitle("Tic-Tac-Toe");
         frame.setSize(1240,800);
         frame.setLayout(new GridLayout(3,3));
-
         JPanel[] panel = new JPanel[9];
         JButton[] buttons = new JButton[9];
+        int[] board = new int[9];
+
         for(int i = 0; i<9; i++){
+            board[i] = 0;
             panel[i] = new JPanel();
             buttons[i] = new JButton("Click");
             buttons[i].setPreferredSize(new Dimension(100, 100));
@@ -33,7 +36,7 @@ public class TicTacToe{
                 public void actionPerformed(ActionEvent e){
                     for(int i = 0; i<9; i++){
                         if (e.getSource() == buttons[i]){ // checks if the button is the one clicked
-                            draw(i, panel);
+                            draw(i, panel, board);
                         }
                     }
                 }
@@ -43,17 +46,35 @@ public class TicTacToe{
         frame.setVisible(true);
     }
 
-    public void draw(int i, JPanel[] panel){
+    public void draw(int user, JPanel[] panel, int[] board){
 
         //users turn
-        panel[i].removeAll();
-        panel[i].updateUI();
-        JLabel label = new JLabel("x");
-        label.setFont(new Font("Sans-Serif", Font.PLAIN, 120));
-        panel[i].add(label);
+        board[user] = 1;
+        panel[user].removeAll();
+        panel[user].updateUI();
+        JLabel label1 = new JLabel("x");
+        label1.setFont(new Font("Sans-Serif", Font.PLAIN, 120));
+        panel[user].add(label1);
 
         //computers turn
-        
+        int computer = -1;
+        for(int j = 0; j<9; j++){
+            if(board[j] == 0){
+                computer = j;
+            }
+        }
+
+        board[computer] = -1;
+        panel[computer].removeAll();
+        panel[computer].updateUI();
+        JLabel label2 = new JLabel("o");
+        label2.setFont(new Font("Sans-Serif", Font.PLAIN, 120));
+        panel[computer].add(label2);
+
+    }
+
+    public static int checkWin(int[] board){
+
     }
 
     public static void main(String args[]) throws FileNotFoundException{
