4a5b4072f76875330b32e963b6b7359606489ab2
Thomas Bueler <twbueler@gmail.com>
2018-11-15 07:54:01 -0900

    done with ex4

--- chap14/Ex4.java
+++ chap14/Ex4.java
@@ -16,17 +16,28 @@ public class Ex4{
 
         JPanel subsetTop = new JPanel(new BorderLayout());
         subsetTop.add(new JButton("1"), BorderLayout.WEST);
-        subsetTop.add(new JTextBox("text"), BorderLayout.CENTER);
+        subsetTop.add(new JTextField("text"), BorderLayout.CENTER);
 
         JPanel top = new JPanel(new GridLayout(3,1));
         top.add(new JButton("2"));
         top.add(subsetTop);
         top.add(new JButton("3"));
 
+        JPanel subset1 = new JPanel(new BorderLayout());
+        subset1.add(new JButton("5"),BorderLayout.SOUTH);
+
+        JPanel subset2 = new JPanel(new FlowLayout());
+        subset2.add(new JButton("6"));
+        subset2.add(new JButton("7"));
+
         JPanel center = new JPanel(new GridLayout(2,2));
+        center.add(new JButton("4"));
+        center.add(subset1);
+        center.add(subset2);
+        center.add(new JButton("8"));
 
         JPanel bottom = new JPanel(new BorderLayout());
-        bottom.add(new Jlabel("Pretty Tricky"), BorderLayout.WEST);
+        bottom.add(new JLabel("Pretty Tricky"), BorderLayout.WEST);
 
         frame.add(center, BorderLayout.CENTER);
         frame.add(top, BorderLayout.NORTH);
