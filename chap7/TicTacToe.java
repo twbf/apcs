c3d641179eeecd89cbba9c468fb0653f501738b7
Thomas Bueler <twbueler@gmail.com>
2018-11-19 21:51:33 -0900

    changing redundancy

--- chap7/TicTacToe.java
+++ chap7/TicTacToe.java
@@ -11,28 +11,30 @@ import javax.swing.event.*;
 
 public class TicTacToe{
 
-    private JFrame frame;
-    private JPanel big;
-    private TicTacToe.BoardPanel b;
-    private int[][] board;
-    private JPanel[][] panel;
-    private JButton[][] buttons;
-
     public static void main(String args[]) throws FileNotFoundException{
         TicTacToe.BoardPanel b = new TicTacToe().new BoardPanel();
     }
 
     public class BoardPanel extends JFrame{
+
+        private JLabel label;
+        private TicTacToe.BoardPanel b;
+        private int[][] board;
+        private JPanel[][] panel;
+        private JButton[][] buttons;
+
         public BoardPanel(){
-            //TicTacToe b = new TicTacToe();
             JFrame frame = new JFrame();
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // so it closes nicely
             frame.setTitle("Tic-Tac-Toe");
             frame.setSize(1240,800);
             frame.setLayout(new GridLayout(3,3));
-            JPanel[][] panel = new JPanel[3][3];
-            JButton[][] buttons = new JButton[3][3];
-            int[][] board = new int[3][3];
+
+            this.panel = new JPanel[3][3];
+            this.buttons = new JButton[3][3];
+            this.board = new int[3][3];
+            this.label = new JLabel();
+
             for(int i = 0; i<3; i++){
                 for(int j = 0; j<3; j++){
                     board[i][j] = 0;
@@ -42,34 +44,26 @@ public class TicTacToe{
                     panel[i][j].add(buttons[i][j]);
                     frame.add(panel[i][j]);
                     buttons[i][j].addActionListener(new Button());
-                    System.out.println("h");
                 }
             }
             frame.setVisible(true);
         }
+
         public class Button implements ActionListener{
             public void actionPerformed(ActionEvent e){
                 for(int i = 0; i<3; i++){
                     for(int j = 0; j<3; j++){
-                        if (e.getSource() == buttons[i][j]){ // checks if the button is the one clicked
-                            draw(i,j);
+                        if (e.getSource() == BoardPanel.this.buttons[i][j]){ // checks if the button is the one clicked
+                            BoardPanel.this.draw(i,j);
                         }
                     }
                 }
             }
         }
 
-        public void draw(int i, int j){
-            //i,j are users cordinaties wheras ci,cj are computer coudinates
-
-
+        public void draw(int i, int j){ //i,j are users cordinaties wheras ci,cj are computer coudinates
             //users turn
-            board[i][j] = 1;
-            panel[i][j].removeAll();
-            panel[i][j].updateUI();
-            JLabel label1 = new JLabel("x");
-            label1.setFont(new Font("Sans-Serif", Font.PLAIN, 120));
-            panel[i][j].add(label1);
+            movePiece(i,j,true);
 
             //computers turn
             int ci = -1;
@@ -82,15 +76,23 @@ public class TicTacToe{
                     }
                 }
             }
-
-            board[ci][cj] = -1;
-            panel[ci][cj].removeAll();
-            panel[ci][cj].updateUI();
-            JLabel label2 = new JLabel("o");
-            label2.setFont(new Font("Sans-Serif", Font.PLAIN, 120));
-            panel[ci][cj].add(label2);
+            movePiece(ci,cj,false);
         }
 
+        public void movePiece(int i, int j, boolean user){
+            if(user){
+                board[i][j] = 1;
+                label = new JLabel("x");
+            } else {
+                board[i][j] = -1;
+                label = new JLabel("o");
+            }
+            label.setFont(new Font("Sans-Serif", Font.PLAIN, 120));
+            panel[i][j].removeAll();
+            panel[i][j].updateUI();
+            panel[i][j].add(label);
+        }
+        
         //public static int checkWin(int[] board){
 
         //}
