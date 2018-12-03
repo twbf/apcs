65fe44afac584e0f73bbd08a56e9a1d6d58cfbb5
Thomas Bueler <twbueler@gmail.com>
2018-12-03 08:38:48 -0900

    directed network

---
+++ ml/java/DirectedNetwork.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.border.*;
+import javax.swing.event.*;
+
+public class DirectedNetwork{
+
+    public static void main(String args[]) throws FileNotFoundException{
+        int[][] grouping = new int[6][];
+        grouping[0] = {1,2}; // 0 likes 1 and two
+        grouping[1] = {1,2};
+        grouping[2] = {1,2};
+        grouping[3] = {0,2};
+        grouping[4] = {2,5};
+        grouping[5] = {2,3};
+    }
+}
