8b8014149307817b9f1276e20076bdf7c724deec
Thomas Bueler <twbueler@gmail.com>
2019-04-02 12:20:08 -0800

    done with magpie

--- Magpie/Magpie2.java
+++ Magpie/Magpie2.java
@@ -22,6 +22,8 @@ public class Magpie2
 		return "Hello, let's talk.";
 	}
 
+    private String statement = " ";
+
 	/**
 	 * Gives a response to a user statement
 	 *
@@ -29,36 +31,53 @@ public class Magpie2
 	 *            the user statement
 	 * @return a response based on the rules given
 	 */
-	public String getResponse(String statement)
+	public String getResponse(String state)
 	{
+        statement = state;
+        statement = statement.toLowerCase();
 		String response = "";
-		if (statement.indexOf("no") >= 0){
+		if (check("no")){
 			response = "Why so negative?";
 		}
-		else if (statement.indexOf(" mother") >= 0
-				|| statement.indexOf(" father") >= 0
-				|| statement.indexOf(" sister") >= 0
-				|| statement.indexOf(" brother") >= 0){
+		else if (check("mother")
+				|| check("sister")
+				|| check("brother")
+				|| check("father")){
 			response = "Tell me more about your family.";
-		} else if (statement.matches(".*(\s)dog(\W|\s).*")){
+		} else if (check("dog")){
 			response = "Tell me more about your pets.";
-		} else if (statement.indexOf(" Mr. Scurr") >= 0){
+		} else if (check("Mr. Scrurr")){
 			response = "he sounds like a great teacher";
 		}  else if (statement.trim().length() == 0){
 			response = "SAY SOMETHING";
-		} else if (statement.indexOf(" shirt") >= 0){
+		} else if (check("shirt")){
 			response = "Tell me more about your clothing";
-		} else if (statement.indexOf(" sad") >= 0){
+		} else if (check("sad")){
 			response = "Don't be";
-		} else if (statement.indexOf(" happy") >= 0){
+		} else if (check("happy")){
 			response = "Nice!";
-		}
+		} else if(statement.length()>7){
+            if (statement.indexOf("i want") >= 0){
+                response = "Would you really be happy if you had " + statement.substring(7);
+            } else if (statement.indexOf("i like") >= 0){
+                response = "What do you like about " + statement.substring(7);
+            }
+        }
 		else
 		{
 			response = getRandomResponse();
 		}
+        if (response.equals("")){
+            response = "Really?";
+        }
+
 		return response;
 	}
+
+
+    private boolean check(String word){
+        return statement.matches(".*"+word+"(\\W|$).*");
+    }
 
 	/**
 	 * Pick a default response to use if nothing else fits.
@@ -66,25 +85,9 @@ public class Magpie2
 	 */
 	private String getRandomResponse()
 	{
-		final int NUMBER_OF_RESPONSES = 6;
+        String[] responces  = { "Interesting, tell me more", "Hmmm.", "Do you really think so?", "You don't say.", "Wow, I don't know", "Tell me."};
 		double r = Math.random();
-		int whichResponse = (int)(r * NUMBER_OF_RESPONSES);
-		String response = "";
-
-		if (whichResponse == 0){
-			response = "Interesting, tell me more.";
-		} else if (whichResponse == 1){
-			response = "Hmmm.";
-		} else if (whichResponse == 2){
-			response = "Do you really think so?";
-		} else if (whichResponse == 3){
-			response = "You don't say.";
-		} else if (whichResponse == 4){
-			response = "Wow, I don't know";
-		} else if (whichResponse == 5){
-			response = "Tell me.";
-		}
-
-		return response;
+		int whichResponse = (int)(r * responces.length);
+		return responces[whichResponse];
 	}
 }
